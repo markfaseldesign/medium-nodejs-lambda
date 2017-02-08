e15e7f5d2ae856d48c30e06f4ae6ea406be6c91a
markfasel <mark@markfaseldesign.com>
2017-02-08 15:01:53 -0500

    update to index
    dynamic username

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ exports.handler = function(event, context, callback) {
 
 	callback = context.done;
 	var data = event.bodyJson || {};
-	
-	var url = "https://medium.com/@markfasel/latest?format=json";
+	var username = 'username'
+	var url = "https://medium.com/@"+username+"/latest?format=json";
 
 	request({
 	    url: url,
@@ -12,7 +12,7 @@ exports.handler = function(event, context, callback) {
 	}, function (error, response, body) {
 		
 	    if (!error && response.statusCode === 200) {
-		    jsonBody = JSON.parse(body.replace('])}while(1);</x>', ''));
+		jsonBody = JSON.parse(body.replace('])}while(1);</x>', ''));
 	        console.log(jsonBody) // Print the json response
 	        callback(null, jsonBody);
 	    }
